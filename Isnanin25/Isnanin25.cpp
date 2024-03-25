510695f6673bba843d2abd93d3f96c5ce3062103
nopriterbang <novalpriya@gmail.com>
2024-03-25 14:37:06 +0700

    membuat display

--- Isnanin25/Isnanin25.cpp
+++ Isnanin25/Isnanin25.cpp
@@ -33,5 +33,17 @@ float pengurangan(float a, float b)
 
 int main()
 {
+	int pilihan;
 
+	do
+	{
+		cout << "==============" << endl;
+		cout << "MENU" << endl;
+		cout << "1. Penjumlahan " << endl;
+		cout << "2. Pembagian "  << endl;
+		cout << "3. Perkalian " << endl;
+		cout << "4. Pengurangan " << endl;
+		cout << "5.Exit " << endl;
+		cout << "Masukan pilihan 1/2/3/4/5"
+	}
 }
